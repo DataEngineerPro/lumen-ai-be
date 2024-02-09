3f4a1d57be619d1c9162df79f78b7742f7613ae2
DataEngineerPro <149567238+DataEngineerPro@users.noreply.github.com>
2024-02-09 14:24:30 +0100

    multiple uploads (#6)
    Co-authored-by: eucloid-fe-dev <149567238+eucloid-fe-dev@users.noreply.github.com>

--- src/demo/controllers/documents.controller.ts
+++ src/demo/controllers/documents.controller.ts
@@ -1,22 +1,21 @@
 import { DemoRepository } from 'src/dynamodb/repositories/demo.repository';
 import {
   BadRequestException,
-  Body,
   Controller,
   Get,
   Logger,
   NotFoundException,
   Param,
   Post,
-  Put,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { FileS3Serivce } from '../services/files3.service';
 import { ApiConsumes, ApiBody } from '@nestjs/swagger';
-import { Document } from '../models/model';
 import { ExtractionService } from '../services/extraction.service';
+import { diskStorage } from 'multer';
+import * as fs from 'fs';
 
 @Controller('documents')
 export class DocumentsController {
@@ -25,7 +24,9 @@ export class DocumentsController {
     private readonly repository: DemoRepository,
     private readonly fileService: FileS3Serivce,
     private readonly extractionService: ExtractionService,
-  ) {}
+  ) { }
+
+
 
   @Get(':id')
   async get(@Param('id') id: string): Promise<any> {
@@ -68,52 +69,55 @@ export class DocumentsController {
       },
     },
   })
-  @UseInterceptors(FileInterceptor('file'))
+
+
+
+  @UseInterceptors(FilesInterceptor('files', 100, {
+    storage: diskStorage({
+      destination: (req:any, file, cb) => {
+        const id = req.params.id;
+        const path = `./raw/${id}`
+        fs.mkdirSync(path, { recursive: true })
+        return cb(null, path)
+      },
+      filename: (req: any, file: Express.Multer.File, callback: (error: Error | null, filename: string) => void) => {
+        const { originalname } = file;
+        const sanitizedName = originalname
+          .replace(/[^a-z0-9]/gi, '_')
+          .toLowerCase();
+        callback(null, `${sanitizedName}`);
+      }
+    }),
+    fileFilter: (req: Express.Request, file: Express.Multer.File, callback: (error: Error | null, acceptFile: boolean) => void) => {
+      const { mimetype, size } = file;
+      if (size > 3 * 1024 * 1024) {
+        callback(null, false);
+      }
+      else if (
+        mimetype !== 'application/pdf' &&
+        mimetype !== 'image/jpeg' &&
+        mimetype !== 'image/png'
+      ) {
+        callback(null, false);
+      }
+      else
+        callback(null, true);
+    }
+  }))
   async create(
-    @UploadedFile('file') file: Express.Multer.File,
+    @UploadedFiles() files: Array<Express.Multer.File>,
     @Param('id') id: string,
   ): Promise<any> {
-    const { originalname, mimetype, buffer } = file;
-    const sanitizedName = originalname
-      .replace(/[^a-z0-9]/gi, '_')
-      .toLowerCase();
-    if (file.size > 3 * 1024 * 1024) {
-      return new BadRequestException("File size can't be more than 3MB");
-    }
-    if (
-      file.mimetype !== 'application/pdf' &&
-      file.mimetype !== 'image/jpeg' &&
-      file.mimetype !== 'image/png'
-    ) {
-      return new BadRequestException('File type can only be pdf, jpeg or png');
-    }
-    const s3Response = await this.fileService.uploadFile(file, id);
-    if (s3Response) {
-      let s3Uri = `s3://${this.fileService.Bucket}/${id}/${sanitizedName}`;
-      this.logger.log(s3Uri);
-      let documentsData: { [k: string]: any } = {};
-      documentsData['master'] = {
-        url: s3Uri,
-        page: 'master',
-      };
-      if (file.mimetype === 'application/pdf') {
-        return this.extractionService.processPDF(s3Uri).then(async (data) => {
-          this.logger.log(data);
-          const pagesToProcess = data.length > 25 ? 25 : data.length;
-          for (let i = 0; i < pagesToProcess; i++) {
-            documentsData[data[i].page] = data[i];
-          }
-          this.logger.log(documentsData);
-          return this.repository.bulkLoadDocuments(documentsData, id);
-        });
-      }
-      return this.extractionService.processImage(s3Uri).then((data) => {
-        this.logger.log(data);
-        documentsData[data.page] = data;
-        return this.repository.bulkLoadDocuments(documentsData, id);
-      });
-    }
+    files.forEach(file => {
 
-    return null;
+      console.log(file);
+    });
+    
+
+    // TODO : Process Files and Add to MongoDB
+
+    return id;
   }
+
+
 }
